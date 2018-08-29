a07fa8ef637c4777118f8a72a72f124475674cb3
Abdulkhamid <abdulkhamid.mumin1@gmail.com>
2018-08-29 23:41:15 +0300

    Misunderstood the task. Changed task 3

--- week2/ex3.c
+++ week2/ex3.c
@@ -1,24 +1,16 @@
 #include <stdio.h>
-long fun(int y)
-{
-    int z;
-    long result = 1;
- 
-    for( z = 1 ; z <= y ; z++ )
-        result = result*z;
- 
-    return ( result );
-}
+
 int main()
 {
-    int n = 6;
+    int n;
+    scanf("%d", &n);
     int x, z;
     for ( x = 0 ; x < n ; x++ )
     {
         for ( z = 0 ; z <= ( n - x - 2 ) ; z++ )
             printf(" ");
         for( z = 0 ; z <= x ; z++ )
-            printf("%ld ",fun(x)/(fun(z)*fun(x-z)));
+            printf("%c", "*");
  
         printf("\n");
     }
