31ae84a2fc42492e755d0bf09abe8b53c5909ac3
Abdulkhamid Muminov <abdulkhamid.uzbek@gmail.com>
2018-09-12 22:53:28 +0300

    Update ex4.c

--- week4/ex4.c
+++ week4/ex4.c
@@ -2,14 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main()
+int main() 
 {
-    char cmd[99];
-    while(1)
-    {   
-        printf(">");
-        gets(cmd);
-        system(cmd);
-    }
-    return 0;
+	char cmd[99];
+	while(1) 
+    {
+		printf(">");
+		gets(cmd, sizeof(cmd), stdin);
+		char *address = strchr(cmd, "&"); //detects '&' sign to run command in 'bg'
+		if (address) 
+        {
+				*address = '\0';
+				int pid = fork();
+				if (!pid) system(cmd);
+		} else 
+        {
+			system(cmd);
+		}
+	}
+
+	return 0;
 }
